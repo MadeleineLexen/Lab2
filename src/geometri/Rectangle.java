3368c9d8888581f9ddc1f81eab5eaa1de75dd0e6
MadeleineLexen <lexen@student.chalmers.se>
2016-11-21 08:34:32 +0100

    uppdaterade Square och Rect

--- src/geometri/Rectangle.java
+++ src/geometri/Rectangle.java
@@ -1,7 +1,83 @@
 package geometri;
 
+import java.awt.*;
+
 /**
  * Created by madeleine on 2016-11-18.
  */
-public class Rectangle {
+public class Rectangle implements GeometricalForm {
+
+    private int height;
+    private int width;
+
+    private Rectangle(int height, int width){
+        this.height = height;
+        this.width = width;
+    }
+    public Rectangle(int x, int y, int width, int height, Color c) throws IllegalPositionException{
+        this.height = height;
+        this.width = width;
+    }
+    public Rectangle(GeometricalForm f, int width, int height, Color c){
+        this.height = height;
+        this.width = width;
+    }
+
+
+
+
+    @Override
+    public int getArea() {
+        return 0;
+    }
+
+    @Override
+    public int compareTo(GeometricalForm f) {
+        return 0;
+    }
+
+    @Override
+    public void fill(Graphics g) {
+
+    }
+
+    @Override
+    public Color getColor() {
+        return null;
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+
+    @Override
+    public int getX() {
+        return 0;
+    }
+
+    @Override
+    public int getY() {
+        return 0;
+    }
+
+    @Override
+    public void move(int dx, int dy) throws IllegalPositionException {
+
+    }
+
+    @Override
+    public int getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public void place(int x, int y) throws IllegalPositionException {
+
+    }
 }
--- src/geometri/Square.java
+++ src/geometri/Square.java
@@ -1,7 +1,83 @@
 package geometri;
 
+import com.sun.xml.internal.ws.encoding.SwACodec;
+
+import java.awt.*;
+
 /**
  * Created by madeleine on 2016-11-18.
  */
-public class Square {
+public class Square implements GeometricalForm {
+    private int side;
+
+    private Square(int length){
+        this.side = side;
+    }
+
+    public Square(int x, int y, int side, Color c) throws IllegalPositionException{
+        this.side = side;
+    }
+    public Square(GeometricalForm f, int side, Color c){
+        this.side = side;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     * @return
+     */
+
+    @Override
+    public int getArea() {
+        return 0;
+    }
+
+    @Override
+    public int compareTo(GeometricalForm f) {
+        return 0;
+    }
+
+    @Override
+    public void fill(Graphics g) {
+    }
+
+    @Override
+    public Color getColor() {
+        return null;
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+
+    @Override
+    public int getX() {
+        return 0;
+    }
+
+    @Override
+    public int getY() {
+        return 0;
+    }
+
+    @Override
+    public void move(int dx, int dy) throws IllegalPositionException {
+
+    }
+
+    @Override
+    public int getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public void place(int x, int y) throws IllegalPositionException {
+
+    }
 }
